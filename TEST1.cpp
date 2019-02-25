179ed21cc2288361f7a9c6d8d79aaa282b6c18b7
sahapop2019 <47093806+sahapop2019@users.noreply.github.com>
2019-02-25 09:20:20 +0700

    Initial commit

---
+++ TEST1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+	void hello()
+	{
+		printf("\nHello C Language");
+	}
+	main()
+	{
+	char name;
+/*hello();
+	hello();
+	hello();*/
+	
+	printf("\n I Love Programming");
+}
